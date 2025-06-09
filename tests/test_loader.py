8bfdfbbc44c3b8547b68c47bef1e64e950a53d21
mariagcdf <maria.garcia.cruz.de.felipe72@gmail.com>
2025-06-09 20:15:27 +0200

    test: added a small realistic test for load_csv function

---
+++ tests/test_loader.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from src.bsm2tools.loader import load_csv
+
+def test_load_csv_realistic():
+    # Simula un pequeño dataset como el de BSM2
+    data = {
+        'Time': [0, 15, 30],
+        'Q': [18465.7, 18392.5, 18402.0],
+        'TSS': [2985.4, 2992.1, 3000.0],
+        'SALK': [4.2, 4.3, 4.1],
+        'TN': [32.5, 33.0, 32.7],
+        'SNH': [15.3, 15.0, 15.5],
+        'Temp': [17.2, 17.4, 17.5]
+    }
+    df_expected = pd.DataFrame(data)
+
+    # Guardamos como un CSV temporal
+    test_csv_path = 'test_bsm2.csv'
+    df_expected.to_csv(test_csv_path, index=False)
+
+    # Cargar usando tu función
+    df_loaded = load_csv(test_csv_path)
+
+    # Verificar que los datos coinciden
+    pd.testing.assert_frame_equal(df_loaded, df_expected)
